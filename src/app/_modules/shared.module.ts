601319d8fd8f7615d0674bfd6537410867843d2b
Lomlomm <vanessaserna60@gmail.com>
2023-11-14 07:44:48 -0600

    seccion-09 member card

--- src/app/_modules/shared.module.ts
+++ src/app/_modules/shared.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ToastrModule } from 'ngx-toastr';
-
-
+import { TabsModule } from 'ngx-bootstrap/tabs';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule, 
+    TabsModule.forRoot(), 
     ToastrModule.forRoot(
       {
         timeOut: 100000, 
@@ -18,7 +18,8 @@ import { ToastrModule } from 'ngx-toastr';
     ),
   ], 
   exports: [
-    ToastrModule
+    ToastrModule, 
+    TabsModule, 
   ]
 })
 export class SharedModule { }
